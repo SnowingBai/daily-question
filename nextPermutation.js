dc03a727ee50021f91a15971c67ec8685ffdc158
baix@shumei.ai <baix@shumei.ai>
2020-11-11 17:04:22 +0800

    下一个排列

---
+++ nextPermutation.js
@@ -0,0 +1,39 @@
+/**
+ * 下一个排列
+ * @param {number[]} nums 
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const nextPermutation = function(nums) {
+  let len = nums.length
+  let i = len - 2
+
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+    i--
+  }
+
+  if (i >= 0) {
+    let j = len - 1
+    while (j >= 0 && nums[i] >= nums[j]) {
+      j--
+    }
+    [nums[i], nums[j]] = [nums[j], nums[i]]
+  }
+
+  reverse(nums, i + 1)
+}
+
+function reverse(nums, start) {
+  let left = start, right = nums.length - 1
+  while (left < right) {
+    [nums[left], nums[right]] = [nums[right], nums[left]]
+    left++
+    right--
+  }
+}
+
+// 思路：
+// 逆向遍历，找到元素索引i，使得 A[i] < A[i + 1]
+// 逆向遍历，找到元素索引j，使得 A[i] < A[j]
+// 交换A[i] 和 A[j]
+// 将剩余部分[i + 1, end)升序排序
+// 找到索引i，剩余部分一定是单调递减的，所以需要将剩余部分收尾交换 [left, right] = [right, left]
